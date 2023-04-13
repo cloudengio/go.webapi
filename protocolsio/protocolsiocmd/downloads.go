dd5bffdf94ab4ecc3e132a04128754dec0b57a69
Cosmos Nicolaou <cnicolaou@cloudeng.io>
2023-04-13 11:33:52 -0700

    .

--- protocolsio/protocolsiocmd/downloads.go
+++ protocolsio/protocolsiocmd/downloads.go
@@ -7,7 +7,6 @@ package protocolsiocmd
 import (
 	"fmt"
 	"os"
-	"path/filepath"
 
 	"cloudeng.io/file/content"
 	"cloudeng.io/webapi/operations"
@@ -27,11 +26,7 @@ func ReadDownload(path string) (*content.Object[protocolsiosdk.ProtocolPayload, 
 	return obj, nil
 }
 
-// WriteDownload writes the downloaded object to the specified path.
 func WriteDownload(path string, obj content.Object[protocolsiosdk.ProtocolPayload, operations.Response]) error {
-	if err := os.MkdirAll(filepath.Dir(path), 0755); err != nil {
-		return err
-	}
 	if err := obj.WriteObjectFile(path, content.GOBObjectEncoding, content.GOBObjectEncoding); err != nil {
 		fmt.Printf("failed to write: %v as %v: %v\n", obj.Value.Protocol.ID, path, err)
 	}
