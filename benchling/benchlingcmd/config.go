a331cd9f1ee2a26bcb9da92ccaa9475edcbd0b12
Cosmos Nicolaou <cnicolaou@cloudeng.io>
2024-02-14 14:54:19 -0800

    .

--- benchling/benchlingcmd/config.go
+++ benchling/benchlingcmd/config.go
@@ -20,15 +20,11 @@ type Auth struct {
 }
 
 type Service struct {
-	ServiceURL string `yaml:"service_url" cmd:"benchling service URL, typically https://altoslabs.benchling.com/api/v2/"`
-	//UsersSort        benchlingsdk.ListUsersParamsSort    `yaml:"users_sort" cmd:"sort order for users, typically name:asc"`
-	UsersPageSize int `yaml:"users_page_size" cmd:"number of users in each page of results, typically 50"`
-	//EntriesSort      benchlingsdk.ListEntriesParamsSort  `yaml:"entries_sort" cmd:"sort order for entries, typically name:asc"`
-	EntriesPageSize int `yaml:"entries_page_size" cmd:"number of entries in each page of results, typically 50"`
-	//FoldersSort      benchlingsdk.ListFoldersParamsSort  `yaml:"folders_sort" cmd:"sort order for folders, typically name:asc"`
-	FoldersPageSize int `yaml:"folders_page_size" cmd:"number of folders in each page of results, typically 50"`
-	//ProjectsSort     benchlingsdk.ListProjectsParamsSort `yaml:"projects_sort" cmd:"sort order for projects, typically modifiedAt"`
-	ProjectsPageSize int `yaml:"projects_page_size" cmd:"number of projects in each page of results, typically 50"`
+	ServiceURL       string `yaml:"service_url" cmd:"benchling service URL, typically https://altoslabs.benchling.com/api/v2/"`
+	UsersPageSize    int    `yaml:"users_page_size" cmd:"number of users in each page of results, typically 50"`
+	EntriesPageSize  int    `yaml:"entries_page_size" cmd:"number of entries in each page of results, typically 50"`
+	FoldersPageSize  int    `yaml:"folders_page_size" cmd:"number of folders in each page of results, typically 50"`
+	ProjectsPageSize int    `yaml:"projects_page_size" cmd:"number of projects in each page of results, typically 50"`
 }
 
 type Config apicrawlcmd.Crawl[Service]
