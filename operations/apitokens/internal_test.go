5b165c11a8fd9e0fe8c8d7e72bb15fd93d9bde35
Cosmos Nicolaou <cnicolaou@cloudeng.io>
2024-03-01 17:11:24 -0800

    .

---
+++ operations/apitokens/internal_test.go
@@ -0,0 +1,9 @@
+// Copyright 2024 cloudeng llc. All rights reserved.
+// Use of this source code is governed by the Apache-2.0
+// license that can be found in the LICENSE file.
+
+package apitokens
+
+func CorruptToken(t *T) {
+	t.value[0] = 'x'
+}
