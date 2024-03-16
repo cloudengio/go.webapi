4850ad239ee1c4eeab3a2571aeb66f272371826e
Cosmos Nicolaou <cnicolaou@cloudeng.io>
2024-03-16 14:41:33 -0700

    .

---
+++ apis/biorxiv/internal/util.go
@@ -0,0 +1,23 @@
+// Copyright 2024 cloudeng llc. All rights reserved.
+// Use of this source code is governed by the Apache-2.0
+// license that can be found in the LICENSE file.
+
+package internal
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func AsInt64(v any) (int64, error) {
+	if v == nil {
+		return 0, nil
+	}
+	switch v := v.(type) {
+	case int:
+		return int64(v), nil
+	case string:
+		return strconv.ParseInt(v, 10, 64)
+	}
+	return 0, fmt.Errorf("unexpected type: %T", v)
+}
